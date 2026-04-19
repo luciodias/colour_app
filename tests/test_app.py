e4f02388240341c7c6c5af0aebf1f45ac55a0f4e
Lucio Dias <luciodiass@gmail.com>
2026-04-19 09:32:10 -0300

    refactor tests to improve structure and readability

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,15 +1,25 @@
+import os
+
 async def test_index(client):
     res = await client.get("/")
     assert res.status_code == 200
     assert b"Sistema IoT" in res.body
 
+async def test_save_config(client,monkeypatch):
+    res = await client.get("/")
+    monkeypatch.setattr(os.path, 'exists', lambda path: False)  # Simulate file not exists
+    res = await client.get("/data")
+    assert res.status_code == 200
+
 async def test_static_ok(client):
     res = await client.get("/static/styles.css")
     assert res.status_code == 200
 
+
 async def test_static_nok(client):
     res = await client.get("/static/..")
     assert res.status_code == 404
+
 
 async def test_dashboard(client):
     res = await client.get("/dashboard")
@@ -34,10 +44,12 @@ async def test_config_data_get(client):
     data = res.json
     assert "update_interval" in data
 
+
 async def test_config_get(client):
     res = await client.get("/config")
     assert res.status_code == 200
     assert "Configurações".encode() in res.body
+
 
 async def test_config_post(client):
     payload = {"update_interval": 2, "temp_threshold": 35, "humidity_threshold": 80}
@@ -46,9 +58,11 @@ async def test_config_post(client):
     assert res.status_code == 200
     assert res.json["status"] == "ok"
 
+
 async def test_config_post_exception(client):
     res = await client.post("/config")
     assert res.status_code == 400
+
 
 async def test_reset_config(client):
     res = await client.post("/reset-config")
